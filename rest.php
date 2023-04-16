f8bc8d58a92d27e5d8566c844948bac7e4952941
Bjorn Zschernack <dergoldbroiler@gmail.com>
2023-04-16 13:45:25 +0200

    functional update and scnd version

--- rest.php
+++ rest.php
@@ -1,6 +1,6 @@
 <?php
 function add_custom_api(){
-register_rest_route('wp/v2', '/posts', array(
+register_rest_route('wp/v2', '/custom', array(
   'methods' => 'GET', 
   'callback' => 'query_posts_and_pages_with_params' 
 ) );
@@ -17,8 +17,47 @@ function query_posts_and_pages($arg){
       'order' => 'DESC',
       'orderby' => 'date',
   );
-  return get_posts($args);
+  return fetchByPostTypeAndTax($args);
 }
+
+function fetchByPostTypeAndTax($args) {
+    // Run a custom query
+    $meta_query = new WP_Query($args);
+    if($meta_query->have_posts()) {
+        //Define an empty array
+        $data = array();
+        // Store each post's data in the array
+        while($meta_query->have_posts()) {
+            $meta_query->the_post();
+            $id = get_the_ID();
+            $post = get_post($id);
+            $link = get_permalink($id);
+            $featured_image = get_the_post_thumbnail_url($id);
+            $post_object = (object) [
+                'id' => $post->ID,
+                'title' => (object) ['rendered' => $post->post_title],
+                'date' => $post->post_date,
+                'slug' => $post->post_name,
+                'link' => $link,
+                'featured_img_url' => $featured_image,
+                'image' => get_the_post_thumbnail_url($post->ID),
+                'excerpt' => (object) ['rendered' => get_the_excerpt()],
+                'type' => get_post_type( $post->ID)
+            ];
+            $data[] = $post_object;
+        }
+        // Return the data
+        return $data;
+    } else {
+        // If there is no post
+        return 'No post to show';
+    }
+  }
+
+  
+  
+  
+  
 
 
 add_action( 'rest_api_init', 'add_custom_api');
--- template-parts/content/content-page.php
+++ template-parts/content/content-page.php
@@ -11,12 +11,65 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-<div class="entry-content">
-	<form>
+<style>
+	.hidden {
+		display:none
+	}
+	.inputcontainer {display:flex;
+	
+	}
+.icon-container {
+	margin-left:-50px;
+	padding-top:15px
+}
+.loader {
+  position: relative;
+  height: 20px;
+  width: 20px;
+  display: inline-block;
+  animation: around 5.4s infinite;
+}
 
-		<input type="text" id="filter-input" placeholder="Stichwort">
-	</form>
+@keyframes around {
+  0% {
+    transform: rotate(0deg)
+  }
+  100% {
+    transform: rotate(360deg)
+  }
+}
+
+.loader::after, .loader::before {
+  content: "";
+  background: white;
+  position: absolute;
+  display: inline-block;
+  width: 100%;
+  height: 100%;
+  border-width: 2px;
+  border-color: #333 #333 transparent transparent;
+  border-style: solid;
+  border-radius: 20px;
+  box-sizing: border-box;
+  top: 0;
+  left: 0;
+  animation: around 0.7s ease-in-out infinite;
+}
+
+.loader::after {
+  animation: around 0.7s ease-in-out 0.1s infinite;
+  background: transparent;
+}</style>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<div class="entry-content" style="position:relative">
+	
+	<div class="inputcontainer">
+    <input type="text" id="filter-input" placeholder="Stichwort">
+    <div class="icon-container hidden">
+      <i class="loader"></i>
+    </div>
+  </div>
 
 
 <div id="results"></div>
@@ -30,35 +83,116 @@
 
 	let result = [];
 
-	const fetchSingle = async (value) => {
-		let pages = `https://foobar:8890/wp-json/wp/v2/pages?s=${value}`;
-		let posts = `https://foobar:8890/wp-json/wp/v2/posts?s=${value}`;
+	/* one fetch and filtering on json data */
+	const fetchOnceAndFilter = async (value) => {
+		let posts = `https://foobar:8890/wp-json/wp/v2/custom`;
 
+		if(!result.posts) {
+			let fetched = await fetch(posts);
+			let json = await fetched.json();
 		
-		let pages_fetch = await fetch(pages);
-		let pages_json = await pages_fetch.json();
-		result['pages']=pages_json;
+			result['posts'] = json;
+		}
+		
+		let returnable_content = result.posts.filter(element => element.title.rendered.toLowerCase().search(value.toLowerCase()) > -1);
+		let returnable = '';
 
-		let posts_fetch = await fetch(posts);
-		let posts_json = await posts_fetch.json();
-		result['posts']=posts_json;
-		
-		if(value.length){	
-			return result.posts.map(element => {
-				
-				return `${element.post_title}<br>`
+		returnable_content.forEach(element => {
+				returnable += '<a>' + element.title.rendered + '</a><hr>';
 			})
-		} 
+				
+		return returnable	
+	}
+
+	/* fetching on keyup event */
+	const fetchAll = async (value) => {
+		let filtered_posts = `https://foobar:8890/wp-json/wp/v2/custom?s=${value}`;
+
+		/*
+
+		$post_object = (object) [
+                'id' => $post->ID,
+                'title' => (object) ['rendered' => $post->post_title],
+                'date' => $post->post_date,
+                'slug' => $post->post_name,
+                'link' => $link,
+                'featured_img_url' => $featured_image,
+                'image' => get_the_post_thumbnail_url($post->ID),
+                'excerpt' => (object) ['rendered' => get_the_excerpt()],
+                'type' => get_post_type( $post->ID)
+            ];
+
+		*/
+		let returnable = '';
+
+		try{
+			let fetched = await fetch(filtered_posts);
+			let json = await fetched.json();
 		
-		return '';
+			result['filtered_content'] = json;
+		
+		
+			result.filtered_content.forEach(element => {
+				returnable += '<a>' + element.title.rendered + '</a><hr>';
+			})
+
+		} catch {
+			returnable = '<span class=alert alert-danger>Keine Ergebnisse</span>';
+		}
+		
+		return returnable;
+	
 		
 	}
 
-	let inp = document.querySelector('#filter-input');
+	const handleSpinner = (action) => {
+		let spinner = document.querySelector('.icon-container');
+		action === 'on' ? spinner.classList.remove('hidden') : spinner.classList.add('hidden')
+	}
 
-	inp.addEventListener('keyup',e => {
-		fetchSingle(e.target.value).then(data => document.querySelector('#results').innerHTML = data);
+	let input = document.querySelector('#filter-input');
+
+	/*input.addEventListener('keyup',e => {
+
+		let searchvalue = e.target.value;
+		let resultcontainer = document.querySelector('#results');
+		
+		let len = searchvalue.length;
+
+		console.log('Laenge', searchvalue.length)
+
+		if(len % 2 === 0 && len >= 2) {
+			handleSpinner('on')
+			fetchAll(e.target.value).then(data => { resultcontainer.innerHTML = data; handleSpinner('off')})
+		}
+		if(len < 2) {
+			resultcontainer.innerHTML = '';
+			handleSpinner('off')
+		}
+		
+	}); */
+
+	input.addEventListener('keyup',e => {
+		
+		let searchvalue = e.target.value;
+		let resultcontainer = document.querySelector('#results');
+
+		let len = searchvalue.length;
+
+		console.log('Laenge', searchvalue.length)
+		
+		if(len % 2 === 0 && len >= 2) {
+			handleSpinner('on')
+			fetchOnceAndFilter(e.target.value).then(data => { resultcontainer.innerHTML = data;handleSpinner('off') })
+		}
+		if(len < 2) {
+			resultcontainer.innerHTML = '';
+			handleSpinner('off')
+		}
+
 	});
+
+
 
 
 
